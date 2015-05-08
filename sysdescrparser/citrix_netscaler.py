b815017b44b624eb6b5d40bb7f9495154f7d5820
mtoshi <mtoshi.g@gmail.com>
2015-05-08 10:18:49 +0900

    Add citrix netscaler.

---
+++ sysdescrparser/citrix_netscaler.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+"""sysdescrparser.citrix_netscaler."""
+
+
+import re
+from sysdescr import SysDescr
+
+
+# pylint: disable=no-member
+class CitrixNetscaler(SysDescr):
+
+    """Class CitrixNetscaler.
+
+    SNMP sysDescr for Citrix Netscaler.
+
+    """
+
+    def parse(self):
+        """Parse."""
+        vendor = 'citrix'
+        os = 'netscaler'
+        series = self.UNKNOWN
+        version = self.UNKNOWN
+
+        regex = (r'^NetScaler\s+(.*:\s+Build\s+.*),\s+Date:\s+')
+        pat = re.compile(regex)
+        res = pat.search(self.raw)
+        if res:
+            version = res.group(1)
+            return self._store(vendor=vendor,
+                               os=os,
+                               series=series,
+                               version=version)
+        return False
