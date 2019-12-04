b0169b7b24b847b8d3ae9c7580af613624d26a2c
yzdann <1543472-yzdann@users.noreply.gitlab.com>
2019-12-04 11:11:04 +0330

    fix bug for not showing ubuntu lts version in string with ubuntu, and also model, version fix

--- sysdescrparser/linux.py
+++ sysdescrparser/linux.py
@@ -19,19 +19,18 @@ class Linux(SysDescr):
 
     versions = {
         "ubuntu": {
-            "ubuntu": "",
             "lucid": "10.04",
             "precise": "12.04",
             "trusty": "14.04",
             "xenial": "16.04",
-            "bionic": "18.04"
+            "bionic": "18.04",
         },
         "centos": {
-            "centos": "",
             "centos6": "6.0",
             "el6": "6.0",
             "centos7": "7.0",
-            "el7": "7.0"
+            "el7": "7.0",
+            "centos": "",
         }
     }
 
@@ -45,6 +44,9 @@ class Linux(SysDescr):
 
     def parse(self):
         """Parsing for sysDescr value."""
+        version_name = ""
+        kernel_version = ""
+
         regex = (r'^Linux\s+')
         pat = re.compile(regex)
         res = pat.search(self.raw)
@@ -56,11 +58,16 @@ class Linux(SysDescr):
                 kernel_version = res[2]
 
             ubuntu_version_names_regex = os_version_names_regex(self.__class__.versions["ubuntu"])
-            ubuntu_match = re.search(
+            ubuntu_lts_match = re.search(
                             ubuntu_version_names_regex,
                             self.raw,
                             flags=re.IGNORECASE
                            )
+            ubuntu_main_version_name_match = re.search(
+                                                "ubuntu",
+                                                self.raw,
+                                                flags=re.IGNORECASE
+                                            )
 
             centos_version_names_regex = os_version_names_regex(self.__class__.versions["centos"])
             centos_match = re.search(
@@ -69,18 +76,22 @@ class Linux(SysDescr):
                             flags=re.IGNORECASE
                            )
 
-            version_name = ""
-            if ubuntu_match:
+            if ubuntu_lts_match or ubuntu_main_version_name_match:
                 self.vendor = "CANONICAL"
                 self.os = "UBUNTU"
-                version_name = ubuntu_match.group()
+
+                if not ubuntu_lts_match:
+                    version_name = self.UNKNOWN
+
+                else:
+                    version_name = ubuntu_lts_match.group()
 
             if centos_match:
                 self.vendor = "CENTOS"
                 self.os = "CENTOS"
                 version_name = centos_match.group()
 
-            if version_name:
+            if version_name and version_name != 'UNKNOWN':
                 # make it lower cause our versions dict keys are lower
                 os_lookup_name = self.os.lower()
                 os_versions = self.__class__.versions[os_lookup_name]
@@ -90,11 +101,15 @@ class Linux(SysDescr):
                 )
                 if version_number:
                     self.version = version_number
-                    self.model = kernel_version
 
                 else:
-                    self.version= kernel_version
+                    self.version = self.UNKNOWN
 
+                self.model = kernel_version if kernel_version else self.UNKNOWN
+
+            elif version_name == 'UNKNOWN':
+                self.version = version_name
+                self.model = kernel_version if kernel_version else self.UNKNOWN
 
             else:
                 self.version = kernel_version
