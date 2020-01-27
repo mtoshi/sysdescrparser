4537b95183501d299f2d5e976f6043bf30083dbe
yzdann <1543472-yzdann@users.noreply.gitlab.com>
2020-01-27 16:39:55 +0330

    update version in setup.py

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ EXCLUDE_FROM_PACKAGES = ['tests']
 
 setup(
     name="snmpsysdescrparser",
-    version="0.1.17",
+    version="0.1.18",
     description='SNMP sysDescr parser.',
     long_description=README,
     author='Yazdan',
