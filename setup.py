1ce5aabb8fab928df9c0ac647cf56025062c286f
yzdann <22779039+yzdann@users.noreply.github.com>
2021-02-02 21:14:23 +0330

    Bump version 0.1.19

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ EXCLUDE_FROM_PACKAGES = ['tests']
 
 setup(
     name="snmpsysdescrparser",
-    version="0.1.18",
+    version="0.1.19",
     description='SNMP sysDescr parser.',
     long_description=README,
     author='Yazdan',
